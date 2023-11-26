4b46e5f21861f4384171a3a028f142597bdb7167
AM\aprij <aprijalmuhaemin199@gmail.com>
2023-11-26 08:59:58 +0700

    choose product

--- cypress/e2e/3-web-magento/2-choose-product/choose-product.cy.js
+++ cypress/e2e/3-web-magento/2-choose-product/choose-product.cy.js
@@ -1,9 +1,21 @@
-const { text } = require("stream/consumers")
-
 describe('Verify Magento choose product Functionality ', () => {
-    it('Success choose product', () => {
-      cy.visit('https://magento.softwaretestingboard.com/').should('have.text', 'LUMA')
+    it('Successfully choose view product', () => {
+      cy.visit('https://magento.softwaretestingboard.com/')
       cy.get(':nth-child(1) > .product-item-info > .product-item-details > .product-item-name > .product-item-link').click()
     })
+
+    it('Successfully choose product with size', () => {
+        cy.visit('https://magento.softwaretestingboard.com/')
+        cy.get('.swatch-opt-1556 > .size > .swatch-attribute-options > #option-label-size-143-item-167').click
+        cy.contains('s')
+        cy.get(':nth-child(1) > .product-item-info > .product-item-details > .product-item-name > .product-item-link').click()
+      })
+
+      it('Successfully choose product with size and color', () => {
+        cy.visit('https://magento.softwaretestingboard.com/')
+        cy.get('.swatch-opt-1556 > .size > .swatch-attribute-options > #option-label-size-143-item-167').click()
+        cy.get('#option-label-color-93-item-50').click().should('have.class', 'option-label="Blue"').click()
+        cy.get(':nth-child(1) > .product-item-info > .product-item-details > .product-item-name > .product-item-link').click()
+      })
 })
   
